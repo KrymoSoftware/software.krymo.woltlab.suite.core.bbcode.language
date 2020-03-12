ded2b68ed32331616d0e6384dfd1af9a00986614
Niklas (Krymonota) <Krymonota@users.noreply.github.com>
2020-03-12 21:28:48 +0100

    Fully implement plugin

---
+++ files/lib/system/bbcode/LanguageBBCode.class.php
@@ -0,0 +1,37 @@
+<?php
+namespace wcf\system\bbcode;
+
+use wcf\system\exception\SystemException;
+use wcf\system\language\LanguageFactory;
+use wcf\system\WCF;
+
+/**
+ * Parses the [lang] bbcode tag.
+ *
+ * @author      Niklas Friedrich Gerstner
+ * @copyright	2020 Krymo Software
+ * @license     Krymo Software - Free Products License <https://krymo.software/licensing-conditions/#free-products>
+ * @package     WoltLabSuite\Core\System\Bbcode
+ */
+class LanguageBBCode extends AbstractBBCode {
+
+    /**
+     * @inheritDoc
+     * @throws SystemException
+     */
+    public function getParsedTag(array $openingTag, $content, array $closingTag, BBCodeParser $parser) {
+        $languageCode = !empty($openingTag['attributes'][0]) ? $openingTag['attributes'][0] : 0;
+
+        if (!$languageCode) {
+            return '';
+        }
+
+        $language = LanguageFactory::getInstance()->getLanguageByCode($languageCode);
+
+        if (!$language || WCF::getLanguage()->languageID != $language->languageID) {
+            return '';
+        }
+
+        return $content;
+    }
+}
